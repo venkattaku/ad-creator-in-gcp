d815cbab4aa7edf8983ef6c22fac4576a9fdfb3e
venkattaku <1440325+venkattaku@users.noreply.github.com>
2025-05-07 07:47:35 +0000

    Update middleware import and requirements for ProxiedHeadersMiddleware

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 # from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
-from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
+# from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
+from fastapi_proxiedheadersmiddleware import ProxiedHeadersMiddleware
 
 # from starlette.middleware.trustedhost import TrustedHostMiddleware as ProxyHeadersMiddleware
 from app.ocr import extract_text
